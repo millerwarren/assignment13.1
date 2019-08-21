bf8fc393b4b18f29cab1fd58fe08e754bc292581
millerwarren <53824450+millerwarren@users.noreply.github.com>
2019-08-21 10:30:33 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+// $('h1').css('color', 'red');
+
+// $('h1').css('backgroundColor', 'gray');
+
+// $('h1').css('fontStyle', 'italic');
+
+// $('h1').css('fontSize', '90px');
+// $('h1').css('color', 'yellow');
+
+
+// var styles = {
+//     backgroundColor: 'blue',
+//     fontSize: '100px',
+//     fontWeight: 'bold',
+//     border: "10px solid red",
+//     textAlign: 'center'
+
+
+// };
+
+// $('h1').css(styles);
+
+// $('p').css('color', 'darkGreen');
+
+// var styles2 = {
+//     backgroundColor: 'pink',
+//     fontSize: '30px',
+//     textAlign: 'left',
+//     fontWeight: 'bold'
+
+// };
+
+// $('p').css(styles2);
+
+$('div').css('backgroundColor', 'purple');
+
+$('.highlight').css('width', '200px');
+
+$('#third').css('border', '8px  solid orange');
